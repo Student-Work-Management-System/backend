6b4a45245ca069edf4717eab77c0b784bb3f01af
nefelibetas <1241142935@qq.com>
2024-03-27 18:02:16 +0800

    添加minio(oss)依赖 完成相关配置, 简单实现其工具类完成上传下载功能 实现FileService所有接口, 文件存在oss中 上传接口后续提供给学生竞赛以及学术作品提供证明材料。

--- src/main/java/edu/guet/studentworkmanagementsystem/service/file/FileService.java
+++ src/main/java/edu/guet/studentworkmanagementsystem/service/file/FileService.java
@@ -1,6 +1,10 @@
 package edu.guet.studentworkmanagementsystem.service.file;
 
+import edu.guet.studentworkmanagementsystem.common.BaseResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface FileService {
     /**
@@ -9,5 +13,11 @@ public interface FileService {
      * @param fileFrom 本系统中需要存的文件来源处只有 <strong>竞赛证明文件</strong> 和 <strong>学术著作文件</strong>
      * @return 文件存放的路径(会根据fileFrom来存储)
      */
-    String upload(MultipartFile file, String fileFrom);
+    BaseResponse<String> upload(MultipartFile file, String fileFrom) throws IOException;
+    /**
+     * 通过文件名下载minio(oss)内的文件
+     * @param filename 文件名
+     * @param response 响应
+     */
+    void download(String filename, HttpServletResponse response);
 }
--- src/test/java/edu/guet/studentworkmanagementsystem/StudentWorkManagementSystemApplicationTests.java
+++ src/test/java/edu/guet/studentworkmanagementsystem/StudentWorkManagementSystemApplicationTests.java
@@ -1,35 +1,14 @@
 package edu.guet.studentworkmanagementsystem;
 
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.jwt.JWT;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.time.Instant;
-import java.util.Date;
-import java.util.UUID;
 
 
 @SpringBootTest
 class StudentWorkManagementSystemApplicationTests {
-    @Autowired
-    private PasswordEncoder passwordEncoder;
     @Test
     void contextLoads() {
-        String key = "$2a$10$EOWyTIhOPiIDaO40I7znfuTmttRRCe3rEzDM9aS9/pWcLe8ZT5fSO";
-        String uuid = UUID.randomUUID().toString();
-        String token = JWT.create()
-                .setPayload("uid", "uid:1")
-                .setJWTId(uuid)
-                .setSubject("StudentWorkManagementSystem")
-                .setIssuer("BridgeFishDev")
-                .setIssuedAt(DateUtil.date())
-                .setExpiresAt(Date.from(Instant.now().plusSeconds(604800)))
-                .setKey(key.getBytes())
-                .sign();
-        System.out.println(key);
-        System.out.println(token);
+
     }
 }
