c454667a1c68c60910f1dbb5a780287a3e2b03f1
nefelibetas <1241142935@qq.com>
2024-04-21 17:58:49 +0800

    style(employment): 去除无用import指令

--- src/main/java/edu/guet/studentworkmanagementsystem/entity/vo/employment/StudentEmploymentVO.java
+++ src/main/java/edu/guet/studentworkmanagementsystem/entity/vo/employment/StudentEmploymentVO.java
@@ -1,18 +1,10 @@
 package edu.guet.studentworkmanagementsystem.entity.vo.employment;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import edu.guet.studentworkmanagementsystem.entity.po.employment.StudentEmployment;
-import edu.guet.studentworkmanagementsystem.entity.po.student.Student;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
