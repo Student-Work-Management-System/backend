dd2ab4978545e721b0391036cd9177a5599331c7
nefelibetas <1241142935@qq.com>
2024-04-21 18:40:03 +0800

    test(employment): 测试修正

--- src/test/java/edu/guet/studentworkmanagementsystem/StudentWorkManagementSystemApplicationTests.java
+++ src/test/java/edu/guet/studentworkmanagementsystem/StudentWorkManagementSystemApplicationTests.java
@@ -1,11 +1,27 @@
 package edu.guet.studentworkmanagementsystem;
 
+import com.mybatisflex.core.paginate.Page;
+import edu.guet.studentworkmanagementsystem.common.BaseResponse;
+import edu.guet.studentworkmanagementsystem.entity.dto.employment.EmploymentQuery;
+import edu.guet.studentworkmanagementsystem.entity.vo.employment.StudentEmploymentVO;
+import edu.guet.studentworkmanagementsystem.service.employment.EmploymentService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StudentWorkManagementSystemApplicationTests {
+    @Autowired
+    private EmploymentService employmentService;
     @Test
     void contextLoads() {
+        EmploymentQuery query = new EmploymentQuery();
+        query.setSearch("");
+        query.setGraduationYear("2022");
+        query.setPageNo(1);
+        query.setPageSize(10);
+        BaseResponse<Page<StudentEmploymentVO>> studentEmployment = employmentService.getStudentEmployment(query);
+        Page<StudentEmploymentVO> data = studentEmployment.getData();
+        System.out.println(data);
     }
 }
