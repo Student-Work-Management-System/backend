5e15e76bcf000e8bcb9191ade90c9988e5c643f1
nefelibetas <1241142935@qq.com>
2024-04-23 13:48:04 +0800

    test(employment): 清空测试

--- src/test/java/edu/guet/studentworkmanagementsystem/StudentWorkManagementSystemApplicationTests.java
+++ src/test/java/edu/guet/studentworkmanagementsystem/StudentWorkManagementSystemApplicationTests.java
@@ -1,21 +1,11 @@
 package edu.guet.studentworkmanagementsystem;
 
-import com.mybatisflex.core.paginate.Page;
-import edu.guet.studentworkmanagementsystem.common.BaseResponse;
-import edu.guet.studentworkmanagementsystem.entity.dto.employment.EmploymentQuery;
-import edu.guet.studentworkmanagementsystem.entity.vo.employment.StudentEmploymentVO;
-import edu.guet.studentworkmanagementsystem.service.employment.EmploymentService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StudentWorkManagementSystemApplicationTests {
-    @Autowired
-    private EmploymentService employmentService;
     @Test
     void contextLoads() {
-        BaseResponse<Object> response = employmentService.deleteStudentEmployment("4");
-        System.out.println(response);
     }
 }
