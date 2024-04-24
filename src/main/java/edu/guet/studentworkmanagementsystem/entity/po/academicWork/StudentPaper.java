a45f63894e6fe79b3d144dd2651e922d0fec9862
nefelibetas <1241142935@qq.com>
2024-04-24 14:44:46 +0800

    fix(all): 删除po中多余的@Id注解
    过多的@Id注解导致deleteById方法生成SQL出现问题

--- src/main/java/edu/guet/studentworkmanagementsystem/entity/po/academicWork/StudentPaper.java
+++ src/main/java/edu/guet/studentworkmanagementsystem/entity/po/academicWork/StudentPaper.java
@@ -4,7 +4,6 @@ import com.mybatisflex.annotation.Column;
 import com.mybatisflex.annotation.Id;
 import com.mybatisflex.annotation.KeyType;
 import com.mybatisflex.annotation.Table;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
