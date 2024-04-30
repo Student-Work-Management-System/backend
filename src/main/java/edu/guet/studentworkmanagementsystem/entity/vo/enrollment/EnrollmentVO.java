69d44c5b565283c1909f2cbab8b8e90620c3957d
nefelibetas <1241142935@qq.com>
2024-04-30 12:20:09 +0800

    fix(enrollment): 修改get请求返回
    原返回Enrollment对象改为EnrollmentVO
    字段差异： enrollMajorId -> enrollMajor
    录取专业id改为录取专业名

---
+++ src/main/java/edu/guet/studentworkmanagementsystem/entity/vo/enrollment/EnrollmentVO.java
@@ -0,0 +1,21 @@
+package edu.guet.studentworkmanagementsystem.entity.vo.enrollment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EnrollmentVO implements Serializable {
+    private String enrollmentId;
+    private String examineeId;
+    private String id;
+    private String name;
+    private String origin;
+    private String enrollMajor;
+    private String firstMajor;
+    private String score;
+    private String enrollTime;
+}
