dcd95b2aba9b27c6b25f8ea574a28c09830e393a
BridgeFishDev <1241142935@qq.com>
2024-05-21 18:24:39 +0800

    fix(async): 修改线程池配置
    读线程池单独配置
    修改原邮件线程池配置

--- src/main/java/edu/guet/studentworkmanagementsystem/config/AsyncConfig.java
+++ src/main/java/edu/guet/studentworkmanagementsystem/config/AsyncConfig.java
@@ -10,21 +10,34 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Configuration
 @EnableAsync
 public class AsyncConfig {
-    private static final int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors();
-    private static final int MAXIMUM_POOL_SIZE = CORE_POOL_SIZE + 1;
-    private static final int QUEUE_CAPACITY = 1000;
-    private static final int KEEP_ALIVE_TIME = 10;
-    private static final String PREFIX = "emailThread-";
+    private static final int CORE_POOL_SIZE = 10;
+    private static final int MAXIMUM_POOL_SIZE = 20;
+    private static final int QUEUE_CAPACITY = 50;
+    private static final int KEEP_ALIVE_TIME = 60;
+    private static final String EMAIL_PREFIX = "emailThread-";
+    private static final String READ_PREFIX = "readThread-";
     private static final ThreadPoolExecutor.CallerRunsPolicy HANDLER = new ThreadPoolExecutor.CallerRunsPolicy();
     @Bean("emailThreadPool")
     public ThreadPoolTaskExecutor emailThreadPool() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setKeepAliveSeconds(KEEP_ALIVE_TIME);
+        executor.setRejectedExecutionHandler(HANDLER);
+        executor.setThreadNamePrefix(EMAIL_PREFIX);
+        executor.initialize();
+        return executor;
+    }
+    @Bean("readThreadPool")
+    public ThreadPoolTaskExecutor readThreadPool() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(CORE_POOL_SIZE);
         executor.setMaxPoolSize(MAXIMUM_POOL_SIZE);
         executor.setQueueCapacity(QUEUE_CAPACITY);
         executor.setKeepAliveSeconds(KEEP_ALIVE_TIME);
         executor.setRejectedExecutionHandler(HANDLER);
-        executor.setThreadNamePrefix(PREFIX);
+        executor.setThreadNamePrefix(READ_PREFIX);
         executor.initialize();
         return executor;
     }
