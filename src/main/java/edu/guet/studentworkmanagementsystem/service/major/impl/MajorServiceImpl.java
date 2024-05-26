a3e844ad26e7f7d4b7c3a64c97781407b473708a
BridgeFishDev <1241142935@qq.com>
2024-05-26 18:52:55 +0800

    fix(major): 获取方法改为使用多线程

--- src/main/java/edu/guet/studentworkmanagementsystem/service/major/impl/MajorServiceImpl.java
+++ src/main/java/edu/guet/studentworkmanagementsystem/service/major/impl/MajorServiceImpl.java
@@ -8,14 +8,25 @@ import edu.guet.studentworkmanagementsystem.exception.ServiceExceptionEnum;
 import edu.guet.studentworkmanagementsystem.mapper.major.MajorMapper;
 import edu.guet.studentworkmanagementsystem.service.major.MajorService;
 import edu.guet.studentworkmanagementsystem.utils.ResponseUtil;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Service
 public class MajorServiceImpl extends ServiceImpl<MajorMapper, Major> implements MajorService {
+    private final ThreadPoolTaskExecutor readThreadPool;
+
+    public MajorServiceImpl(@Qualifier("readThreadPool") ThreadPoolTaskExecutor readThreadPool) {
+        this.readThreadPool = readThreadPool;
+    }
+
     @Override
     @Transactional
     public <T> BaseResponse<T> addMajor(Major major) {
@@ -26,6 +37,7 @@ public class MajorServiceImpl extends ServiceImpl<MajorMapper, Major> implements
             return ResponseUtil.success();
         throw new ServiceException(ServiceExceptionEnum.OPERATE_ERROR);
     }
+
     @Override
     @Transactional
     public <T> BaseResponse<T> updateMajor(Major major) {
@@ -36,10 +48,27 @@ public class MajorServiceImpl extends ServiceImpl<MajorMapper, Major> implements
             return ResponseUtil.success();
         throw new ServiceException(ServiceExceptionEnum.OPERATE_ERROR);
     }
+
     @Override
     public BaseResponse<List<Major>> getMajors() {
-        return ResponseUtil.success(mapper.selectAll());
+        CompletableFuture<BaseResponse<List<Major>>> future = CompletableFuture.supplyAsync(() -> ResponseUtil.success(mapper.selectAll()), readThreadPool);
+        try {
+            return future.get(3, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            Throwable cause = e.getCause();
+            switch (cause) {
+                case ServiceException serviceException ->
+                        throw serviceException;
+                case TimeoutException ignored ->
+                        throw new ServiceException(ServiceExceptionEnum.GET_RESOURCE_TIMEOUT);
+                case InterruptedException ignored ->
+                        throw new ServiceException(ServiceExceptionEnum.GET_RESOURCE_INTERRUPTED);
+                default ->
+                        throw new ServiceException(ServiceExceptionEnum.OPERATE_ERROR);
+            }
+        }
     }
+
     @Override
     @Transactional
     public <T> BaseResponse<T> deleteMajor(String majorId) {
