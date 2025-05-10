5742acf19e80fdb476c3733423e7350d49a9c048
nefelibetas <1241142935@qq.com>
2025-05-10 11:54:48 +0800

    fix: 修改mapper层

--- src/main/java/edu/guet/studentworkmanagementsystem/mapper/povertyAssistance/PovertyAssistanceMapper.java
+++ src/main/java/edu/guet/studentworkmanagementsystem/mapper/povertyAssistance/PovertyAssistanceMapper.java
@@ -3,7 +3,6 @@ package edu.guet.studentworkmanagementsystem.mapper.povertyAssistance;
 import com.mybatisflex.core.BaseMapper;
 import edu.guet.studentworkmanagementsystem.entity.dto.povertyAssistance.PovertyAssistanceStatQuery;
 import edu.guet.studentworkmanagementsystem.entity.po.povertyAssistance.PovertyAssistance;
-import edu.guet.studentworkmanagementsystem.entity.vo.povertyAssistance.PovertyAssistanceStatGroup;
 import edu.guet.studentworkmanagementsystem.entity.vo.povertyAssistance.PovertyAssistanceStatRow;
 
 import java.util.List;
